4b4307aec8462f9e0ddf98d33efd7d40c48f9949
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2023-02-27 19:33:16 +0530

    Updated JS

---
+++ Script.js
@@ -0,0 +1,4 @@
+document.getElementById("Heading").addEventListener("click",function(){
+    document.getElementById("Para").innerHTML="You clicked the heading";
+    document.getElementById("Heading").style.color="red";
+})
