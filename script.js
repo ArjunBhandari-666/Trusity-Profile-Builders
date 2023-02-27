75706207b6859ac1830d0aa044aef210efc1f7f2
BuilderGeorgio <123478702+BuilderGeorgio@users.noreply.github.com>
2023-02-27 19:02:29 +0400

    Added Java script
    I've added a java script to make the website a responsive one. Just testing java script so its very basic

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("Heading").addEventListener("click",function(){
+    document.getElementById("Heading").innerHTML="My name is Georgio Hans";
+    document.getElementById("Heading").style.color="red";
+})
