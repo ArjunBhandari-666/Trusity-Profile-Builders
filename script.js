88c062a96e2aa9c2ba2bc969f277afbebdb422de
Arjun Bhandari <72136655+ArjunBhandari-666@users.noreply.github.com>
2023-03-06 19:30:06 +0530

    JS Creating elements

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById("Heading").addEventListener("click",function(){
     document.getElementById("Heading").innerHTML="My name is Georgio Hans";
     document.getElementById("Heading").style.color="red";
 })
+var p=document.createElement("p")
+p.innerHTML="CLICK Above"
+document.getElementById("Heading").appendChild(p);
+
